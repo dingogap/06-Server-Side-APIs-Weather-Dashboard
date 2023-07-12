ac220dd4456f5933c4989c01753da28bdf69ab79
Peter Medbury <peter.medbury@dingogap.net.au>
2023-07-12 13:44:42 +1000

    updating scope

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # My Weather Dashboard
 5 Day Weather Forecast Ap 
+
+
+
+
+
+## Resources Used
+1. Bootcamp Materials - Lesson Material from Weeks 1 - 8
+2. Prior knowlege and experience
+3. Group tutorial sessions
+4. [jQuery ][def2] jQuery API Documentation
+5. [Day.js][def3] Day.js Documentation
+6. [Materialize][def4] Documentation
+7. [Open Weather API][def5] Documentation
+8. [MDM Web Docs][def6] Attribute Selectors
+
+
+
+
+[def1]: https://dingogap.github.io/Weather-Dashboard/
+[def2]: https://jquery.com/
+[def3]: https://day.js.org/en/
+[def4]: https://materializecss.com/
+[def5]: https://openweathermap.org/api
+[def6]: https://developer.mozilla.org/en-US/docs/Web/CSS/Attribute_selectors
+
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,15 @@
 // Globals
-var city
-var state = ""
-var country = "Au"
-var owUrl = "https://api.openweathermap.org"
-var owApiKey = "fd223c8245ed1b33feec8296469d3041";
+const owUrl = "https://api.openweathermap.org";
+const owApiKey = "fd223c8245ed1b33feec8296469d3041";
 
-var owLocationData = {};
-var owCurrentData = {}
-var forecastData = {}
+/* var locationData = {}; */
+var currentData = {};
+var forecastData = {};
 
+// Click handler for search button - won't work until the page is fully loaded
 $(document).ready(function () {
     $("#search-btn").click(function () {
-        city = $("#city-name").val().trim()
+        let city=$("#city-name").val().trim().replaceAll(" ", "%20");
         firstDataLookup(city);
     });
 });
@@ -23,11 +21,19 @@ $("#city-name").on("keypress", function (event) {
     }
 });
 
+// Event Handler for clicking in the City Name Input Field
+$("#city-name").click(function () {
+    $("#city-name").val("");
+  });
+  
+
 // Builds the query string to search for the city and return lat/long
 function firstDataLookup(city) {
-    var location = city + "," + state + "," + country
-    owLocationData = owUrl + "/geo/1.0/direct?q=" + location + "&limit=1&apikey=" + owApiKey;
-    firstAPICall(owLocationData);
+    const state = "";
+    const country = "Au";
+    const location = city + "," + state + "," + country;
+    const searchString = owUrl + "/geo/1.0/direct?q=" + location + "&limit=1&apikey=" + owApiKey;
+    firstAPICall(searchString);
 }
 
 // Search ow for city location data
@@ -41,22 +47,26 @@ function firstAPICall(queryString) {
             return response.json();
         })
         .then(function (data) {
-            var locationInfo
-            locationInfo = data[0]
             firstDataSave(data[0]);
-            secondDataLookup(owLocationData.lat, owLocationData.lon)
+            secondDataLookup(data[0].lat, data[0].lon);
         });
-
 }
 
 // Builds the Query String to reteive current weather in metric
 function secondDataLookup(lat, lon) {
-    owGeoData = owUrl + "/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + owApiKey + "&units=metric"
+    const owGeoData =
+        owUrl +
+        "/data/2.5/weather?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=" +
+        owApiKey +
+        "&units=metric";
     secondAPICall(owGeoData);
 }
 
 function secondAPICall(queryString) {
-
     fetch(queryString, {
         method: "GET",
         credentials: "same-origin",
@@ -66,22 +76,26 @@ function secondAPICall(queryString) {
             return response.json();
         })
         .then(function (data) {
-            var currentData;
-            currentData = data
-            secondDataSave(currentData)
-            thirdDataLookup(owLocationData.lat, owLocationData.lon)
+            secondDataSave(data);
+            thirdDataLookup(locationData.lat, locationData.lon);
         });
-
 }
 
 // Builds the Query String to reteive current weather in metric
 function thirdDataLookup(lat, lon) {
-    owGeoData = owUrl + "/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + owApiKey + "&units=metric"
+    owGeoData =
+        owUrl +
+        "/data/2.5/forecast?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=" +
+        owApiKey +
+        "&units=metric";
     thirdAPICall(owGeoData);
 }
 
 function thirdAPICall(queryString) {
-
     fetch(queryString, {
         method: "GET",
         credentials: "same-origin",
@@ -91,36 +105,39 @@ function thirdAPICall(queryString) {
             return response.json();
         })
         .then(function (data) {
-            var currentData;
-            currentData = data
-            thirdDataSave(currentData)
+            thirdDataSave(data);
         });
-
 }
 
 function firstDataSave(apiData) {
-    owLocationData = apiData
-    console.log(owLocationData)
-/*     console.log(owLocationData)
-    console.log(owLocationData.name)
-    console.log(owLocationData.country)
-    console.log(owLocationData.lat)
-    console.log(owLocationData.lon) */
-
+    locationData = apiData;
+    console.log(locationData);
+    /*     console.log(locationData)
+      console.log(locationData.name)
+      console.log(locationData.country)
+      console.log(locationData.lat)
+      console.log(locationData.lon) */
 }
 
-function secondDataSave(apiData) {
-    owCurrentData = apiData
-    console.log(owCurrentData)
-    weatherIcon = "https://openweathermap.org/img/wn/" + owCurrentData.weather[0].icon + ".png"
-    $("#crnt-city").text(owCurrentData.name + " (" + dayjs.unix(owCurrentData.dt).format('D/M/YYYY') + ")")
-    $("#crnt-city").append("<img class='reponsive-img' src=" + weatherIcon + ">")
-    $("#crnt-temp").text("Temp: " + owCurrentData.main.temp + "°C")
-    $("#crnt-wind").text("Wind: " + owCurrentData.wind.speed + " km/h")
-    $("#crnt-humidity").text("Humidity: " + owCurrentData.main.humidity + " %")
+function secondDataSave(currentData) {
+    console.log(currentData);
+    weatherIcon =
+        "https://openweathermap.org/img/wn/" +
+       currentData.weather[0].icon +
+        ".png";
+    $("#crnt-city").text(
+       currentData.name +
+        " (" +
+        dayjs.unix(currentData.dt).format("D/M/YYYY") +
+        ")"
+    );
+    $("#crnt-city").append("<img class='reponsive-img' src=" + weatherIcon + ">");
+    $("#crnt-temp").text("Temp: " +currentData.main.temp + "°C");
+    $("#crnt-wind").text("Wind: " +currentData.wind.speed + " km/h");
+    $("#crnt-humidity").text("Humidity: " +currentData.main.humidity + " %");
 }
 
 function thirdDataSave(apiData) {
-    forecastData=apiData
-    console.log (forecastData)
+    console.log(apiData);
+    console.log(locationData);
 }
